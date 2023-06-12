2a832b298f96f3398b331ab95025e3c6ad7299db
Misha <misha.oldenburger@gmail.com>
2023-06-12 19:33:04 +0200

    update

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Misha#0460 =)'
+ui_page "html/index.html"
+client_scripts {
+    "client/*.lua"
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+files {
+    "html/script.js",
+    "html/style.css",
+    "html/index.html"
+}
